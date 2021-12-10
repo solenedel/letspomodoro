b6c806229fc0f37c95d2e3955fbe446c528038c5
solenedel <solene98@student.ubc.ca>
2021-12-10 10:24:46 -0800

    style timer options form

--- src/components/TimerOptionsForm.js
+++ src/components/TimerOptionsForm.js
@@ -5,15 +5,22 @@ const TimerOptionsForm = ({ className }) => {
   return (
     <section className={className} id="timer-optn-form">
       <form action="submit">
-        {/* eslint-disable-next-line */}
-        <label htmlFor="focus-period">Focus period (minutes): </label>
-        <input type="number" id="focus-period" name="focus-period" />
-        {/* eslint-disable-next-line */}
+        <div className="form-optn">
+          {/* eslint-disable-next-line */}
+        <label htmlFor="focus-period">Focus period: </label>
+          <input type="number" id="focus-period" name="focus-period" />
+          mins
+        </div>
+        <div className="form-optn">
+          {/* eslint-disable-next-line */}
         <label htmlFor="break-period">Break period (minutes): </label>
-        <input type="number" id="break-period" name="break-period" />
-        {/* eslint-disable-next-line */}
+          <input type="number" id="break-period" name="break-period" /> mins
+        </div>
+        <div className="form-optn">
+          {/* eslint-disable-next-line */}
         <label htmlFor="cycles">Number of cycles: </label>
-        <input type="number" id="cycles" name="cycles" />
+          <input type="number" id="cycles" name="cycles" /> mins
+        </div>
       </form>
     </section>
   );
--- src/components/styled-components/TimerOptionsForm.style.js
+++ src/components/styled-components/TimerOptionsForm.style.js
@@ -1,18 +1,36 @@
 /* eslint-disable import/prefer-default-export */
 import styled from 'styled-components';
 import TimerOptionsForm from '../TimerOptionsForm';
+import { colorVars } from './css-variables';
 
 export const StyledTimerOptionsForm = styled(TimerOptionsForm)`
-  font-size: 22px;
-  border: solid blue 3px;
+  font-size: 25px;
+  color: ${colorVars.mainText};
+  width: 80vw;
+  height: 20vh;
+  margin-top: 25px;
+  position: center;
 
-  form {
+  & form {
     display: flex;
-    flex-direction: column;
+    flex-direction: row;
+    flex-wrap: wrap;
+  }
+
+    & label {
+      margin-right: 5px;
+      margin-bottom: 10px;
+    }
 
     & input {
-      width: 50px;
-      height: auto;
+      border: none;
+      border-radius: 5px;
+      padding: 5px;
+      height: 40px;
+      width: 80px;
+      /* margin-right: 50px; */
+      margin-bottom: 10px;
+      font-size: 25px;
     }
   }
 `;
