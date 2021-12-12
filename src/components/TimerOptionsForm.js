445a879fb5bf5e1e30e7f575893e2ab78969bd81
solenedel <solene98@student.ubc.ca>
2021-12-11 16:53:20 -0800

    work on react timer form handling

--- src/components/TimerOptionsForm.js
+++ src/components/TimerOptionsForm.js
@@ -1,7 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 // eslint-disable-next-line
 const TimerOptionsForm = ({ className }) => {
+  const [timerSettings, setTimerSettings] = useState({
+    focusPeriod: '',
+    breakPeriod: '',
+    cycles: '',
+  });
+
+  const handleInputChangeFocus = (e) => {
+    console.log('FOCUS PERIOD: ', e.target.value);
+    setTimerSettings({ ...timerSettings, focusPeriod: e.target.value });
+  };
+
+  const handleInputChangeBreak = (e) => {
+    console.log('BREAK PERIOD: ', e.target.value);
+    setTimerSettings({ ...timerSettings, breakPeriod: e.target.value });
+  };
+
+  const handleInputChangeCycles = (e) => {
+    console.log('CYCLES: ', e.target.value);
+    setTimerSettings({ ...timerSettings, cycles: e.target.value });
+    console.log('TIMER SETTINGS: ', timerSettings);
+  };
+
+  const handleTimerOptnSubmit = (e) => {
+    e.preventDefault();
+
+    if (timerSettings.focusPeriod && timerSettings.breakPeriod && timerSettings.cycles) {
+      console.log('TIMER SETTINGS: ', timerSettings);
+    }
+  };
+
   return (
     <section className={className} id="timer-optn-form">
       <form action="submit">
@@ -12,9 +42,11 @@ const TimerOptionsForm = ({ className }) => {
             type="number"
             min="10"
             max="120"
-            defaultValue="50"
+            // defaultValue="50"
             id="focus-period"
             name="focus-period"
+            value={timerSettings.focusPeriod}
+            onChange={handleInputChangeFocus}
           />{' '}
           minutes
         </div>
@@ -25,19 +57,32 @@ const TimerOptionsForm = ({ className }) => {
             type="number"
             min="5"
             max="60"
-            defaultValue="10"
+            // defaultValue="10"
             id="break-period"
             name="break-period"
+            value={timerSettings.breakPeriod}
+            onChange={handleInputChangeBreak}
           />{' '}
           minutes
         </div>
         <div className="form-optn">
           {/* eslint-disable-next-line */}
         <label htmlFor="cycles">Number of cycles - </label>
-          <input type="number" min="1" max="20" defaultValue="3" id="cycles" name="cycles" /> (one
-          cycle = 1 focus period + 1 break period)
+          <input
+            type="number"
+            min="1"
+            max="20"
+            // defaultValue="3"
+            id="cycles"
+            name="cycles"
+            value={timerSettings.cycles}
+            onChange={handleInputChangeCycles}
+          />{' '}
+          (one cycle = 1 focus period + 1 break period)
         </div>
-        <button type="submit">Confirm</button>
+        <button type="submit" onSubmit={handleTimerOptnSubmit}>
+          Confirm
+        </button>
       </form>
     </section>
   );
